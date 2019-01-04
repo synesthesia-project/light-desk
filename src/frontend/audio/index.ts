b7333724d8d3a809eba5285528ea7ef51c31e54c
Sam Lanning <sam@samlanning.com>
2019-01-04 19:21:08 +0000

    Prepare audio sample list for sharing in frontend+backend

--- src/frontend/audio/index.ts
+++ src/frontend/audio/index.ts
@@ -1,15 +1,5 @@
-interface Sample {
-  file: string;
-  /** How many copies of this audio file should be made ready */
-  poolSize: number;
-  volume: number;
-  /** At what time in the file should playback start */
-  startTime: number;
-}
+import {AudioFile, AUDIO_FILES} from '../../shared/static';
 
-function sample(file: string, poolSize: number, volume: number, startTime: number) {
-  return new AudioPool({file, poolSize, volume, startTime});
-}
 
 /**
  * A fixed-number collection of Audio elements for a single sound file, to allow for multi-playback of a single sound file
@@ -20,7 +10,7 @@ class AudioPool {
   private readonly pool: HTMLAudioElement[] = [];
   private next = 0;
 
-  constructor(sample: Sample) {
+  constructor(sample: AudioFile) {
     this.startTime = sample.startTime;
     for (let i = 0; i < sample.poolSize; i++) {
       const audio = new Audio('/audio/' + sample.file);
@@ -38,17 +28,16 @@ class AudioPool {
   }
 }
 
-const SAMPLES = {
-  touch: sample('freesound/123105__dj-chronos__gui-2.wav', 3, 0.2, 0),
-  beep2: sample('freesound/123112__dj-chronos__gui-8.wav', 3, 0.2, 0.1),
-  beep3: sample('freesound/220206__gameaudio__beep-space-button.wav', 3, 0.6, 0),
-  ready1: sample('freesound/220172__gameaudio__flourish-spacey-2.wav', 1, 0.6, 0),
-  powerUp: sample('freesound/264762__farpro__guiclick.ogg', 1, 0.6, 0),
-  powerDown: sample('freesound/264763__farpro__guiclick2.ogg', 1, 0.6, 0)
-};
+export type SampleKey = keyof typeof AUDIO_FILES;
 
-export type SampleKey = keyof typeof SAMPLES;
+const SAMPLES = new Map<SampleKey, AudioPool>();
 
-export function play(sample: SampleKey) {
-  SAMPLES[sample].play();
+for (const key of Object.keys(AUDIO_FILES)) {
+  const audioFile: AudioFile = AUDIO_FILES[key];
+  SAMPLES.set(key as keyof typeof AUDIO_FILES, new AudioPool(audioFile));
+}
+
+export function play(key: SampleKey) {
+  const sample = SAMPLES.get(key);
+  if (sample) sample.play();
 }
