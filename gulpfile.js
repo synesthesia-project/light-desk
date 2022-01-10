96273b3d548f8526cf491c34f5e197b76cc6b8de
Sam Lanning <sam@samlanning.com>
2022-01-10 16:44:39 +0000

    Update gulpfile following dep updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,36 @@
+var util = require('@synesthesia-project/gulp-util');
 var gulp = require('gulp');
 var clean = require('gulp-clean');
-var gutil = require("gulp-util");
-var ts = require('gulp-typescript');
-var tslint = require('gulp-tslint');
-var runSequence = require('run-sequence');
-var webpack = require('webpack');
 var typedoc = require("gulp-typedoc");
+var webpack = require('webpack');
 
-var frontendTsProject = ts.createProject('src/frontend/tsconfig.json');
-var backendTsProject = ts.createProject('src/backend/tsconfig.json');
-var sharedTsProject = ts.createProject('src/shared/tsconfig.json');
+util.cleanTask(['build', '.tmp']);
 
-// Utility Functions
-
-function handleError(err) {
-  gutil.log("Build failed", err.message);
-  process.exit(1);
-}
-
-gulp.task('clean', function() {
-  return gulp.src(['build', '.tmp'], {read: false})
-        .pipe(clean());
+util.typescriptTasks({
+  prefix: 'frontend-',
+  tsconfig: 'src/frontend/tsconfig.json',
+  outputDir: '.tmp/frontend/',
+  tslintSrc: ['src/frontend/**/*.ts', 'src/frontend/**/*.tsx'],
+  tslintConfig: 'tslint.json'
 });
 
-gulp.task('frontend-ts', function () {
-    return frontendTsProject.src()
-      .pipe(frontendTsProject())
-      .on('error', handleError)
-      .pipe(gulp.dest('.tmp/frontend/'));
+util.typescriptTasks({
+  prefix: 'backend-',
+  tsconfig: 'src/backend/tsconfig.json',
+  outputDir: '.tmp/backend/',
+  tslintSrc: ['src/backend/**/*.ts'],
+  tslintConfig: 'tslint.json'
 });
 
-gulp.task('backend-ts', function () {
-    return backendTsProject.src()
-      .pipe(backendTsProject())
-      .on('error', handleError)
-      .pipe(gulp.dest('.tmp/backend/'));
+util.typescriptTasks({
+  prefix: 'shared-',
+  tsconfig: 'src/shared/tsconfig.json',
+  outputDir: '.tmp/shared/',
+  tslintSrc: ['src/shared/**/*.ts'],
+  tslintConfig: 'tslint.json'
 });
 
-gulp.task('shared-ts', function () {
-    return sharedTsProject.src()
-      .pipe(sharedTsProject())
-      .on('error', handleError)
-      .pipe(gulp.dest('.tmp/shared/'));
-});
-
-gulp.task('tslint', function() {
-  return gulp.src(['src/**/*.ts'])
-  .pipe(tslint({
-    formatter: 'verbose',
-    configuration: 'tslint.json'
-  }))
-  .on('error', handleError)
-  .pipe(tslint.report());
-});
-
-gulp.task("frontend-webpack", ['frontend-ts'], function(callback) {
+gulp.task("frontend-webpack", function(callback) {
     // run webpack
     webpack({
         entry: {
@@ -79,44 +55,60 @@ gulp.task("frontend-webpack", ['frontend-ts'], function(callback) {
     });
 });
 
-gulp.task('backend-copy', ['backend-ts'], function () {
+gulp.task('backend-copy', function () {
     return gulp.src(['.tmp/backend/**/*']).pipe(gulp.dest('build/backend'));
 });
 
-gulp.task('shared-copy', ['shared-ts'], function () {
+gulp.task('shared-copy', function () {
     return gulp.src(['.tmp/shared/**/*']).pipe(gulp.dest('build/shared'));
 });
 
-gulp.task('frontend-audio-copy', ['shared-ts'], function () {
+gulp.task('frontend-audio-copy', function () {
     return gulp.src(['src/frontend/audio/**/*']).pipe(gulp.dest('build/frontend/audio'));
 });
 
-gulp.task('clean-docs', function() {
-  return gulp.src(['docs/api'], {read: false})
+gulp.task('default', gulp.series(
+  'clean',
+  gulp.parallel(
+    'frontend-ts',
+    'backend-ts',
+    'shared-ts'
+  ),
+  gulp.parallel(
+    'frontend-webpack',
+    'backend-copy',
+    'shared-copy',
+    'frontend-audio-copy'
+  )
+));
+
+gulp.task('tslint', gulp.series(
+  gulp.parallel(
+    'frontend-tslint',
+    'backend-tslint',
+    'shared-tslint'
+  )
+));
+
+gulp.task('clean-docs', function () {
+  return gulp.src(['docs/api'], { read: false, allowEmpty: true })
         .pipe(clean());
 });
 
-gulp.task('docs', ['clean-docs'], function() {
+gulp.task('build-docs', function () {
   return gulp
     .src(['src/backend/**/*.ts'])
     .pipe(typedoc({
-        readme: 'src/api-docs-readme.md',
-        module: 'commonjs',
-        target: 'es6',
-        out: 'docs/api',
-        name: 'API Reference - Synesthesia Project Light Desk',
-        exclude: 'src/shared/**/*,src/backend/util/id-map.ts,src/backend/server.ts',
-        // So that the generated output is consistent when the documentation hasn't changed, always use
-        // 'master' as the branch, rather than the sha of the source that was used.
-        gitRevision: 'master',
-        media: 'docs/media'
+      readme: 'src/api-docs-readme.md',
+      out: 'docs/api',
+      name: 'API Reference - Synesthesia Project Light Desk',
+      exclude: 'src/shared/**/*,src/backend/util/id-map.ts,src/backend/server.ts',
+      // So that the generated output is consistent when the documentation hasn't changed, always use
+      // 'master' as the branch, rather than the sha of the source that was used.
+      gitRevision: 'develop',
+      media: 'docs/media',
+      tsconfig: 'src/backend/tsconfig.json'
     }));
 });
 
-gulp.task('default', function(callback) {
-  runSequence(
-    'clean',
-    ['frontend-webpack', 'frontend-audio-copy', 'backend-copy', 'shared-copy'],
-    'tslint',
-    callback);
-});
+gulp.task('docs', gulp.series('clean-docs', 'build-docs'));
